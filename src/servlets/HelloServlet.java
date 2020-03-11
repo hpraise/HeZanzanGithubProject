ee9ddf93bc9f9bfbed4bbe709daedee1668a2d8a
HeZanzan <2679709271@qq.com>
2020-03-11 12:16:41 +0800

    第二次从IDEA进行git提交到本地仓库

--- src/servlets/HelloServlet.java
+++ src/servlets/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("Hello Github Project!");
+        out.println("Hello My Github Project!");
 
     }
 }
